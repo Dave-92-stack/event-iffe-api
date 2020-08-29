b574f07ab6f2790bbffc9b7e2c5c3f43dc410c97
leni lunderman <lenilunderman@gmail.com>
2020-08-28 20:58:16 -0400

    testing rsvp part 1 friday

---
+++ curl-scripts/rsvp/rsvp.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+API="http://localhost:4741"
+URL_PATH="/events"
+curl "${API}${URL_PATH}/${ID}/rsvp" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Bearer ${TOKEN}" \
+echo
