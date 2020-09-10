623c191f10e9205d5f05d5b62846259808604dc4
Dave-92-stack <dave.sher92@gmail.com>
2020-09-09 21:47:36 -0400

    Implemented Multer in route and s3_upload module
    Installed multer package
    Use memory storage to handle incoming image uploads
    Pass file to s3_upload module and use the original name and buffer to upload

--- app/routes/upload_routes.js
+++ app/routes/upload_routes.js
@@ -1,12 +1,21 @@
 const express = require('express')
+const multer = require('multer')
+const storage = multer.memoryStorage()
+const upload = multer({ storage })
 const Upload = require('../models/upload')
 const router = express.Router()
 
-router.post('/uploads', (req, res, next) => {
-  // Time stampa round 1:10
-  Upload.create(req.body.upload)
-    .then(upload => {
-      res.status(201).json({ upload })
+const s3Upload = require('../../lib/s3_upload')
+
+router.post('/uploads', upload.single('upload'), (req, res, next) => {
+  console.log(req.file)
+  s3Upload(req.file)
+    .then(awsFile => {
+      console.log(awsFile)
+      return Upload.create({ url: awsFile.Location })
+    })
+    .then(uploadDoc => {
+      res.status(201).json({ upload: uploadDoc })
     })
     .catch(next)
 })
